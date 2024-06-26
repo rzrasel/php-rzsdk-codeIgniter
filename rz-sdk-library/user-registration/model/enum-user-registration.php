00a70f11fc09786352f31c34d181e56613d0d6d9
Rz Rasel <rzrasel@users.noreply.github.com>
2024-06-26 18:34:18 +0600

    Laptop 2024-03-31 01 (#4)
    System Database Schema Created - 2024-03-31 20:33:51.48
    Add JSON Class Directory Path - 2024-03-31 22:34:54.83
    Update user_login_key database table - 2024-04-01 0:45:19.41

--- rz-sdk-library/user-registration/model/enum-user-registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace RzSDK\Model\User\Registration;
-
-enum UserRegistrationEnum: string {
-    case REGI_ID = "user_regi_id";
-    case MODIFIED_DATE = "modified_date";
-    case CREATE_DATE = "created_date";
-}
-?>
