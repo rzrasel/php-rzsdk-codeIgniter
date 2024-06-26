58bd431ab8e7a3b8ed87acaa8ee76929142d8bf1
Rz Rasel <emailrzrasel@gmail.com>
2024-06-26 18:48:31 +0600

    Add Email And Password Validation Class

--- zz-rz-microservices01/rz-sdk-library/validation/email-validation.php
+++ zz-rz-microservices01/rz-sdk-library/validation/email-validation.php
@@ -6,7 +6,7 @@ class EmailValidation {
     public function __construct() {
     }
 
-    public static function isEmail($email) {
+    public function isEmail($email) {
         //$regex = "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/";
         $regex = "/^[a-z][_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/";
         if(preg_match($regex, $email) && filter_var($email, FILTER_VALIDATE_EMAIL)) {
