c6b25e020f8ebaa288d2c778ff4f8f37d829615c
Rz Rasel <rzrasel@users.noreply.github.com>
2024-06-26 18:37:32 +0600

    System Database Schema Created
    System Database Schema Created

--- rz-sdk-library/user-registration/model/enum-user-registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace RzSDK\Model\User\Registration;
-
-enum UserRegistrationEnum: string {
-    case ID = "user_id";
-    case CREATE_DATE = "created_date";
-    case MODIFIED_DATE = "modified_date";
-}
-?>
